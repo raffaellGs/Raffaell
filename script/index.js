21e3d3ce0d148596b5bcd923c64071e6ca7191f9
Rafael <rafael111515@gmail.com>
2022-03-31 00:04:42 -0300

    Rafael

---
+++ script/index.js
@@ -0,0 +1,6 @@
+//Função para o menu subir quando um item for clicado
+$(document).ready(function(){
+    $(".navbar-nav ").click(function(event){
+      $(".navbar-collapse").collapse('hide');
+    });
+  });
